ea10b54bbadaf4b66b2f213c89d6d414a5e6d109
sofiav <velardiezsofia@gmail.com>
2025-01-27 22:37:45 +0100

    operadores en javascript

---
+++ HolaMundo/03-operadores.js
@@ -0,0 +1,36 @@
+//Operadores Aritméticos
+
+let a = 5
+let b = 10
+
+console.log(a + b)
+console.log(a - b)
+console.log(a * b)
+console.log(a / b)
+
+console.log(a % b)
+console.log(a ** b)
+
+console.log(a++)  // Incremento
+console.log(a--) // Decremento
+
+
+//Operadores de Asignación:
+let myVariable = 2
+console.log(myVariable)
+myVariable +=2
+console.log(myVariable)
+
+myVariable +=2
+myVariable -=2
+myVariable *=2
+myVariable /=2
+myVariable **=2
+
+
+//Operadores de comparación, la comparación de dos variables siempre es de tipo boolean
+console.log(a > b)
+console.log(a < b)
+console.log(a >= b)
+console.log(a <= b)
+console.log(a == b)
