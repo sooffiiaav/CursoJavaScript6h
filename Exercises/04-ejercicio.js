47f5ec92d1e5694e079ca700d0f8f6b026f33051
sofiav <velardiezsofia@gmail.com>
2025-01-27 22:59:06 +0100

    ejercicios de strings sencillos

---
+++ Exercises/04-ejercicio.js
@@ -0,0 +1,15 @@
+// Concatena dos cadenas de texto
+let nombre = 'Sofia'
+let email = 'velardiezsofia@gmail.com'
+ console.log("Tu nombre es" +nombre+ "y tu email "+email)
+
+ //Muestra la longitu de una cadena de texto
+console.log(nombre.length)
+
+//Muestra el primer y ultimo caracter de un string
+console.log(nombre[0])
+console.log(nombre[nombre.length - 1])
+
+// Convierte a mayusculas y minusculas
+console.log(nombre.toLowerCase)
+console.log(nombre.toUpperCase)
